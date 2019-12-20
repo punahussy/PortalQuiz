d52af67b24e279c4733ca4790b7bc7eff77f765b
Artem Ponomarev <cotique+punahussy@users.noreply.github.com>
2019-12-20 14:06:03 +0500

    Started Work

---
+++ GameObjects/FakeWall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portalquiz.GameObjects
+{
+    public class FakeWall : IGameObject
+    {
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public Bitmap Texture => textures.wall;
+
+        public FakeWall(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void OnCollision(Player player)
+        {
+           
+        }
+    }
+}
